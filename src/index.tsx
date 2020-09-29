a51be6ff70d994b266eec3b39e6f4a12171fafd4
Yahevin <Yahevin15@gmail.com>
2020-09-29 13:11:49 +0300

    Add react-redux dependence

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,10 @@ import {BrowserRouter as Router} from "react-router-dom";
 import store from "@/store";
 import "@/global-styles/reset.css";
 import "@/global-styles/base.css";
-import IndexPage from "@/pages/IndexPage";
 
 ReactDOM.render(
     <Provider store={store}>
         <Router basename={''}>
-            <IndexPage/>
         </Router>
     </Provider>,
     document.getElementById('app')
